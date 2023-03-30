8c4557dec15d0fd82af84028bc4b3b3fdab1f668
osparking <jbpark03@gmail.com>
2023-03-30 11:04:19 +0900

    spring boot v-2.7.10 initial

---
+++ src/main/java/space/bumtiger/tarleaf_extras/TarleafExtrasApplication.java
@@ -0,0 +1,13 @@
+package space.bumtiger.tarleaf_extras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarleafExtrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarleafExtrasApplication.class, args);
+	}
+
+}
